47be16873ab73f07eb6501cda72eafea62353fa5
nbazarian <84804518+nbazarian@users.noreply.github.com>
2021-07-27 09:06:19 -0400

    Updated file
    Search function created for Space Events and solar eclipse prediction made with a tolerance of 1 degree.

--- AntikytheraMechanism.cpp
+++ AntikytheraMechanism.cpp
@@ -1,8 +1,14 @@
+//Calculations based on data from: https://www.antikytheramechanism.com/howitworks.html
+//SQL code for opening/closing database and handling query callback source from code by Marisha Rawlins
 #include <iostream>
 #include <sqlite3.h>
 #include <stdio.h>
 #include <string>
-#include "EventClasses.h"
+#include <vector>
+#include <time.h>
+#include <chrono>
+#include <ctime>
+#include <string.h>
 
 using namespace std;
 
@@ -52,24 +58,70 @@ double calcExeglimos(double saros){
 double calcMoon(double solar){
 	return solar * (254.0/19.0);
 }
+
+void calcEclipse(double solar, double  moon){
+	
+	while (solar > 1){
+		solar = solar - 1;
+	}
+
+	while (moon > 13.3684){
+		moon = moon - 13.368421052631578947368421052632;
+	}
+
+	double sunRot = solar * 360;
+	double lunRot = moon * 360;
+	if ((sunRot <= lunRot + 0.037134 && sunRot >= lunRot - 0.037134) ||  (sunRot <= lunRot + 360.037134 && sunRot >= lunRot + 359.62866) ){
+		std::cout << "There is a solar eclipse occurring\n";
+	}
+	else if((sunRot <= lunRot + 180.037134 && sunRot >= lunRot + 179.962866) || (sunRot >= lunRot - 180.037134 && sunRot <= lunRot - 179.962866)){
+		std::cout << "There is a lunar eclipse occurring\n";
+	}
+
+}
+
+tm* returnCurrentDate(){
+	std::time_t now_c = time(0);
+	struct tm *parts = std::localtime(&now_c);
+	return parts;
+}
+
+
 int main(void){
 
 double solarPointer, metonicPointer, callippicPointer, gamesPointer, sarosPointer, exeglimosPointer, moonPointer;
 std::string dbCarry;
 int input;
 
-std::cout << "1. Display information from database\n2.Calculate positions\n3.Close\nSelect:";
+std::cout << "1.Search for event by date\n2.Calculate positions\n3.Close\nSelect:";
 std::cin >> input;
 
 	if (input == 1){
 		sqlite3* DB; 
 		int exit = 0;
     		exit = sqlite3_open("antikythera.db", &DB);			//open the database
-		
 		char* messageError; 
 
-		//Input the SQL commands here.
+			tm* parts = returnCurrentDate();
+    		std::cout << parts->tm_mday  << std::endl;
+    		std::cout <<1 + parts->tm_mon <<std::endl;
 
+		std::cout << "1.Search by current date\n2.Search by different date\nSelect:";
+		std::cin >> input;
+		double tmDay = parts->tm_mon;
+		double tmMon = parts->tm_mday;
+			if (input == 1){
+				std::string temp = "SELECT FROM SpaceEvents WHERE Month = "+  to_string(tmMon) +" AND Day = "+ to_string(tmMon) +";";
+				exit = sqlite3_exec(DB, temp.c_str(), callback, NULL, NULL);
+			}
+			else if(input == 2){
+				std::string month, day;
+				std::cout << "Please enter the month and day:";
+				std::cin >> month >> day;
+
+				string temp = "SELECT FROM SpaceEvents WHERE Month = "+ month +" AND Day = "+ day +";";
+                                exit = sqlite3_exec(DB, temp.c_str(), callback, NULL, NULL);
+			}
 		sqlite3_close(DB); 
 	}
 	else if (input == 2){
@@ -81,8 +133,9 @@ std::cin >> input;
 		sarosPointer = calcSaros(solarPointer);
 		exeglimosPointer = calcExeglimos(sarosPointer);
 		moonPointer = calcMoon(solarPointer);
-		std::cout << "Metonic Pointer=" << metonicPointer << "\nCallippic Pointer=" << callippicPointer << "\nGames Pointer=" << gamesPointer << "\nSaros pointer=" << sarosPointer << std::endl;
-		std::cout << "Exeglimos pointer=" << exeglimosPointer << "\nMoon pointer=" << moonPointer << std::endl;
+		std::cout << "Metonic Pointer=" << metonicPointer << " cycle(s)\nCallippic Pointer=" << callippicPointer << " cycles(s)\nGames Pointer=" << gamesPointer << " cycle(s)\nSaros pointer=" << sarosPointer << " cycle(s)\n";
+		std::cout << "Exeglimos pointer=" << exeglimosPointer << " cycles(s)\nMoon pointer=" << moonPointer << " rotations of the moon\n";
+		calcEclipse(solarPointer, moonPointer);
 	}
 	else if (input == 3)
 		std::cout << "Closing program...\n";
