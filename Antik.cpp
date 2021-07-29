037175df8f74be611b235c0afce25399575afdd8
nezadm <84913231+nezadm@users.noreply.github.com>
2021-07-28 20:14:49 -0400

    Add files via upload

---
+++ Antik.cpp
@@ -0,0 +1,61 @@
+CREATE TABLE antik (
+    CelEvent text PRIMARY KEY,
+    CelDate text NOT NULL,
+    CelDist text NULL
+)    
+
+Insert INTO spaceevents VALUES ('Quadrantid Meteor Shower','January 2-3', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'January 13', NULL);
+Insert INTO spaceevents VALUES ('Mercury at Greatest Eastern Elongation', 'January 24', NULL);
+Insert INTO spaceevents VALUES ('Full Moon', 'January 28', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'February 11', NULL);
+Insert INTO spaceevents VALUES ('Full Moon', 'February 27', NULL);
+Insert INTO spaceevents VALUES ('Mercury at Greatest Western Elongation', 'March 6', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'March 13', NULL);
+Insert INTO spaceevents VALUES ('March Equinox', 'March 20', NULL);
+Insert INTO spaceevents VALUES ('Full Moon', 'March 28', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'April 12', NULL);
+Insert INTO spaceevents VALUES ('Lyrids Meteor Shower', 'April 22, 23', NULL);
+Insert INTO spaceevents VALUES ('Full Moon, Supermoon', 'April 27', NULL);
+Insert INTO spaceevents VALUES ('Eta Aquarids Meteor Shower', 'May 6, 7' , NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'May 11', NULL);
+Insert INTO spaceevents VALUES ('Mercury at Greatest Eastern Elongation', 'May 17', NULL);
+Insert INTO spaceevents VALUES ('Full Moon, Supermoon', 'May 26', NULL);
+Insert INTO spaceevents VALUES ('Total Lunar Eclipse', 'May 26', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'June 10', NULL);
+Insert INTO spaceevents VALUES ('Annual Solar Eclipse', 'June 10', NULL);
+Insert INTO spaceevents VALUES ('June Solstice', 'June 21', NULL);
+Insert INTO spaceevents VALUES ('Full Moon, Supermoon', 'June 24', NULL);
+Insert INTO spaceevents VALUES ('Mercury at Greatest Western Elongation', 'July 4', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'July 10', NULL);
+Insert INTO spaceevents VALUES ('Full Moon', 'July 24', NULL);
+Insert INTO spaceevents VALUES ('Delta Aquarids Meteor Shower', 'July 28, 29',NULL);
+Insert INTO spaceevents VALUES ('Saturn at Opposition', 'August 2', NULL);
+Insert INTO spaceevents VALUES ('Perseids Meteor Shower', 'August 12, 13', NULL);
+Insert INTO spaceevents VALUES ('Jupiter at Opposition', 'August 19', NULL);
+Insert INTO spaceevents VALUES ('Full Moon, Blue Moon', 'August 22', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'September 7', NULL);
+Insert INTO spaceevents VALUES ('Neptune at Opposition, Supermoon', 'September 14', NULL);
+Insert INTO spaceevents VALUES ('Mercury at Greatest Eastern Elongation', 'September 14', NULL);
+Insert INTO spaceevents VALUES ('Full Moon', 'September 20', NULL);
+Insert INTO spaceevents VALUES ('September Equinox', 'September 22', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'October 6',NULL);
+Insert INTO spaceevents VALUES ('Draconids Meteor Shower', 'October 7', NULL);
+Insert INTO spaceevents VALUES ('Full Moon', 'October 20' , NULL);
+Insert INTO spaceevents VALUES ('Ornoids Meteor Shower', 'October 21, 22', NULL);
+Insert INTO spaceevents VALUES ('Mercury at Greatest Western Elongation', 'October 25', NULL);
+Insert INTO spaceevents VALUES ('Venus at Greatest Eastern Elongation', 'October 29', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'November 4', NULL);
+Insert INTO spaceevents VALUES ('Taurids Meteor Shower', 'November 4, 5', NULL);
+Insert INTO spaceevents VALUES ('Uranus at Opposition', 'November 5', NULL);
+Insert INTO spaceevents VALUES ('Leonids Meteor Shower', 'November 17, 18', NULL);
+Insert INTO spaceevents VALUES ('Full Moon', 'November 19', NULL);
+Insert INTO spaceevents VALUES ('Partial Lunar Eclipse', 'November 19', NULL);
+Insert INTO spaceevents VALUES ('New Moon', 'December 4' , NULL);
+Insert INTO spaceevents VALUES ('Total Solar Eclipse', 'December 4', NULL);
+Insert INTO spaceevents VALUES ('Geminids Meteor Shower', 'December 13, 14', NULL);
+Insert INTO spaceevents VALUES ('Full Moon', 'December 19', NULL);
+Insert INTO spaceevents VALUES ('December Solstice, Supermoon', 'December 21', NULL);
+Insert INTO spaceevents VALUES ('Ursids Meteor Shower', 'December 21, 22', NULL);
+
+return 0;
